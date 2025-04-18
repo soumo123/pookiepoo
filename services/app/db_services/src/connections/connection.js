1b0fc242e2af2307511d076f05262c01448a374c
Soummya Biswas <soummyabiswas999@gmail.com>
2025-04-18 13:59:54 +0530

    add db

--- services/app/db_services/src/connections/connection.js
+++ services/app/db_services/src/connections/connection.js
@@ -5,9 +5,9 @@ dotenv.config();
 const username = process.env.DB_USERNAME
 const password = process.env.DB_PASSWORD
 
-
+const URL = `mongodb://${username}:${password}@ac-0ktkn7b-shard-00-00.xszhyhm.mongodb.net:27017,ac-0ktkn7b-shard-00-01.xszhyhm.mongodb.net:27017,ac-0ktkn7b-shard-00-02.xszhyhm.mongodb.net:27017/?ssl=true&replicaSet=atlas-diwl40-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
 // const URL = `mongodb://${username}:${password}@ac-0ktkn7b-shard-00-00.xszhyhm.mongodb.net:27017,ac-0ktkn7b-shard-00-01.xszhyhm.mongodb.net:27017,ac-0ktkn7b-shard-00-02.xszhyhm.mongodb.net:27017/?ssl=true&replicaSet=atlas-diwl40-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
-  const URL = `mongodb://127.0.0.1:27017/dating-app`
+  // const URL = `mongodb://127.0.0.1:27017/dating-app`
 const connectToDatabase = async () => {
     try {
       await mongoose.connect(URL, {
